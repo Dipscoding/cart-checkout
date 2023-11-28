fc91b9969d7f64a16d1bbf07eddcfdff714b67e1
Dipscoding <deepeshkumar1908@gmail.com>
2023-11-28 14:19:26 +0530

    till redux

--- src/App.js
+++ src/App.js
@@ -3,53 +3,13 @@ import './App.css';
 import Header from './components/Layout/Header';
 import Subheader from './components/Layout/Subheader';
 import Products from './products/Products';
-import {useState} from 'react'
+
 
 
 
 
 function App() {
 
-  const [cartItems,setCartItems] = useState([])
-  const [eventQueue,setEventQueue] = useState({
-    id:"",
-    type:""
-  })
-
-  const handleAddItem = (item) =>{
-    let items = [...cartItems]
-    let index = items.findIndex(i=>i.id===item.id)
-    if(index > -1){
-      items[index]=item
-    } else{
-      items.push(item)
-
-    }
-    setCartItems([...items])
-  }
-  const handleRemoveItem = (item)=>{
-
-    let items = [...cartItems];
-    let index = items.findIndex(i=>i.id===item.id)
-    if(items[index].quantity===0){
-      items.splice(index,1)
-
-    }
-    else {
-      items[index]=item
-    }
-    setCartItems([...items])
-  }
-
-  const handleEventQueue = (id,type) =>{
-   setEventQueue({
-    id,
-    type
-   })
-
-    }
-
-  
 
   return (
     <div className="App">
